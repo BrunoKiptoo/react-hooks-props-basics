76ca8fc4af4a016e113c7e4cc8c92e216b456267
Bruno <brunochelimo@gmail.com>
2023-01-12 21:54:32 +0300

    done

---
+++ src/components/image.js
@@ -0,0 +1,9 @@
+//import React from "react";
+
+//function Image (){
+    //return <div >
+        //<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiIaLlLx-blmRtqDKil0k9q46PXFO2ums-4GVPBpiYYAS_I9kx9-0_Vi9jpKSO-BJzHr8&usqp=CAU" alt="Girl in a jacket" width="500" height="600"> this girl is om fire</img>
+    //</div>
+//}
+
+//export default Image;
